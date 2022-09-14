167770e03859cdd7236e0d7ef53b09f2660b6bec
pr0n00gler <programmer10110@gmail.com>
2022-09-14 16:46:50 +0300

    fix config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Neutron Docs',
   tagline: 'Neutron',
   url: 'https://neutron-org.github.io',
-  baseUrl: '/',
+  baseUrl: '/neutron-docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
@@ -18,6 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'neutron-org', // Usually your GitHub org/user name.
   projectName: 'neutron-docs', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
