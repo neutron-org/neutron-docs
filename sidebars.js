b4f5cb731055127d27e5c7f76fc66a388eb44d8a
현수영 <soo@DEBRML10052308.localdomain>
2023-07-13 18:22:02 +0900

    adding cosmwasm+remix to tutorials section

--- sidebars.js
+++ sidebars.js
@@ -278,7 +278,8 @@ const sidebars = {
             items: [
                 'tutorials/cosmwasm_ica',
                 'tutorials/cosmwasm_icq',
-                'tutorials/integration_tests'
+                'tutorials/integration_tests',
+                'tutorials/cosmwasm_remix'
             ],
         },
         {
