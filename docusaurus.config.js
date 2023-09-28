9b161c2f6ade19bda7ebcb12b2dad1299aecf26e
pr0n00gler <programmer10110@gmail.com>
2023-09-28 12:55:51 +0300

    major version 1.0 and 1.1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
           lastVersion: 'current',
           versions: {
             current: {
-              label: '1.1.0',
+              label: '1.1',
               path: '/',
             },
           },
